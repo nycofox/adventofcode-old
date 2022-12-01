db586a36fa0e063cae99558ef2ab87530a1a05a3
Anders Pettersen <github@pecon.no>
2022-12-01 09:45:07 +0100

    Day one

---
+++ 1/day1.php
@@ -0,0 +1,24 @@
+<?php
+
+$input = file('input.txt', FILE_IGNORE_NEW_LINES);
+
+$elves = [];
+$rowdata = [];
+
+foreach ($input as $row) {
+    if (empty($row)) {
+        $elves[] = array_sum($rowdata);
+        $rowdata = [];
+    }
+
+    $rowdata[] = (int)$row;
+}
+
+natsort($elves);
+
+$sorted = array_slice($elves, -3, 3);
+//$sorted = array_slice(array_reverse($elves), 0, 3);
+
+print_r('PART ONE: The elf with the most calories has a total of ' . max($elves) . PHP_EOL);
+
+print_r('PART TWO: The three elves with the most calories have a total of ' . array_sum($sorted) . ' calories' . PHP_EOL);
