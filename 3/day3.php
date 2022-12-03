4f67b68b196c374127087a7410b783394d258dc4
nycofox <github@alkis.net>
2022-12-03 12:08:24 +0100

    Day 3 - Part Two

--- 3/day3.php
+++ 3/day3.php
@@ -2,7 +2,8 @@
 
 $input = file('input.txt', FILE_IGNORE_NEW_LINES);
 
-$sum = 0;
+$sum_1 = 0;
+$sum_2 = 0;
 
 foreach ($input as $rucksack) {
     $letters_1 = str_split(substr($rucksack, 0, strlen($rucksack) / 2));
@@ -10,10 +11,19 @@ foreach ($input as $rucksack) {
 
     $common = implode(array_unique(array_intersect($letters_1, $letters_2)));
 
-    $sum += lettervalue($common);
+    $sum_1 += lettervalue($common);
 }
 
-print_r('Part one - Sum of priorities: ' . $sum);
+$groups = array_chunk($input, 3);
+
+foreach ($groups as $group) {
+    $common = implode(array_unique(array_intersect(str_split($group[0]), str_split($group[1]), str_split($group[2]))));
+
+    $sum_2 += lettervalue($common);
+}
+
+print_r('Part one - Sum of priorities: ' . $sum_1 . PHP_EOL);
+print_r('Part two - Sum of priorities: ' . $sum_2 . PHP_EOL);
 
 function lettervalue($letter)
 {
