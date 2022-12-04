d711f8e8b7b930acbdbf4cc71503c7a26676712b
nycofox <github@alkis.net>
2022-12-04 11:19:06 +0100

    Day 4 and cleanup

---
+++ 2022/4/day4.php
@@ -0,0 +1,30 @@
+<?php
+
+$input = file('input.txt', FILE_IGNORE_NEW_LINES);
+
+$fully_overlaps = 0;
+$partial_overlaps = 0;
+
+foreach ($input as $pair) {
+    $elves = explode(',', $pair);
+
+    $elf1 = explode('-', $elves[0]);
+    $elf2 = explode('-', $elves[1]);
+
+    if (
+        ($elf1[0] >= $elf2[0] && $elf1[1] <= $elf2[1]) ||
+        ($elf2[0] >= $elf1[0] && $elf2[1] <= $elf1[1])
+    ) {
+        $fully_overlaps++;
+    }
+
+    if (
+        !(max($elf1) < min($elf2) || min($elf1) > max($elf2)) ||
+        !(max($elf2) < min($elf1) || min($elf2) > max($elf1))
+    ) {
+        $partial_overlaps++;
+    }
+}
+
+print_r('Part one - Fully overlaps: ' . $fully_overlaps . PHP_EOL);
+print_r('Part one - Partial overlaps: ' . $partial_overlaps . PHP_EOL);
